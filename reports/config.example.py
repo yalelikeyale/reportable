5f1e025de250feaa2245d1e5447c7afd4ac36611
adamrdavid <adamrdavid@gmail.com>
2016-01-26 22:24:48 -0800

    update config example

--- reports/config.example.py
+++ reports/config.example.py
@@ -11,26 +11,26 @@ aurora = {
 
 # legacy db
 mysqlcon = {
-    "host": 'db03.cnvxtb0vybkd.us-east-1.rds.amazonaws.com', #'db01.wiser.com',
-    "port": 3306,
-    "username": os.environ['AURORA_USER'],
-    "password": os.environ['AURORA_PASS'],
-    "database": 'wp_data_prod'
+  "host": 'db03.cnvxtb0vybkd.us-east-1.rds.amazonaws.com', #'db01.wiser.com',
+  "port": 3306,
+  "username": os.environ['AURORA_USER'],
+  "password": os.environ['AURORA_PASS'],
+  "database": 'wp_data_prod'
 }
 
 # redshift dwh
 wiser_redshift = {
-   'username': '',
-   'password': '',
-   'host': 'dwh.crt3yyrvkmj5.us-east-1.redshift.amazonaws.com',
-   'database': 'dwh',
-   'raise_on_warnings': True,
-   'port' : 5439
+  'username': '',
+  'password': '',
+  'host': 'dwh.crt3yyrvkmj5.us-east-1.redshift.amazonaws.com',
+  'database': 'dwh',
+  'raise_on_warnings': True,
+  'port' : 5439
 }
 
 ftp_creds = {
-    'username': os.environ['FTP_USER'],
-    'password': os.environ['FTP_PASS'],
+  'username': os.environ['FTP_USER'],
+  'password': os.environ['FTP_PASS'],
   'host': 'ftp.wiser.com',
   'port': '21',
   'path': '/usersftp/LocalUser/%s/'
