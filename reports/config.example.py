1eeab108701708c0d07e28d2bf487627701632bb
adamrdavid <adamrdavid@gmail.com>
2016-01-26 22:26:57 -0800

    update config example

--- reports/config.example.py
+++ reports/config.example.py
@@ -13,8 +13,8 @@ aurora = {
 mysqlcon = {
   "host": 'db03.cnvxtb0vybkd.us-east-1.rds.amazonaws.com', #'db01.wiser.com',
   "port": 3306,
-  "username": os.environ['AURORA_USER'],
-  "password": os.environ['AURORA_PASS'],
+  "username": "",
+  "password": "",
   "database": 'wp_data_prod'
 }
 
@@ -29,20 +29,20 @@ wiser_redshift = {
 }
 
 ftp_creds = {
-  'username': os.environ['FTP_USER'],
-  'password': os.environ['FTP_PASS'],
+  'username': '',
+  'password': '',
   'host': 'ftp.wiser.com',
   'port': '21',
   'path': '/usersftp/LocalUser/%s/'
 }
 
 hulk_creds = {
-  'host': os.environ['HULK_HOST'],
+  'host': '',
   'port': 22,
-  'username': os.environ['HULK_USER'],
-  'password': os.environ['HULK_PASS'],
-  'key': os.environ['HULK_KEY'],
-  'path': os.environ['HULK_PATH']
+  'username': '',
+  'password': '',
+  'key': '',
+  'path': ''
 }
 
 shorturl_api_key = ''
