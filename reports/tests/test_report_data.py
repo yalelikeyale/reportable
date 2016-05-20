f3c830405cfc4aebe746eb1ca222dc56cec8c04a
adamrdavid <adamrdavid@gmail.com>
2016-05-20 11:52:46 -0700

    new deduping option

--- reports/tests/test_report_data.py
+++ reports/tests/test_report_data.py
@@ -3,7 +3,7 @@ import reports
 import MySQLdb
 import pandas as pd
 from reports import config
-from reports import push_report
+#from reports import push_report
 mysqlcon = config.mysqlcon
 
 INPUT_FILE = "sid_uid_test.csv"
@@ -34,7 +34,7 @@ for index, row in stores.iterrows():
   else:
     stores.loc[index, "result"] = "Success!"
     stores.loc[index, "report_data"] = str(report_data)
-    print "Has dup: ", push_report.has_duplicate(report_data)
+    #print "Has dup: ", push_report.has_duplicate(report_data)
     # UNCOMMENT to actually push report to production
     #push_report.create_row(report_data)
 print "printing results...."
